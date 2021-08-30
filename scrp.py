80a0b2d28ad4ed467d0dc122bc870b8f98d828ba
andre-bandeli <andre.lbandeli@gmail.com>
2021-08-30 14:42:47 -0300

    integraçao telegram bot ++ filtro de atualizaçao

--- scrp.py
+++ scrp.py
@@ -1,23 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import telepot
+
+bot_chatID = 'your id'
+bot = telepot.Bot('your code')
 
 page = requests.get("https://empregacampinas.com.br/?s=marketing")
 soup = BeautifulSoup(page.content, 'html.parser')
 lista = []
 lista_aux = [0]
 
-for link in soup.find_all("a", {"class": "thumbnail"}):
-    lista.append(link.get('href'))
+def get_equals(str1, str2):
+    str1 = lista[0]
+    str2 = lista_aux[0]
+    if str1 == str2:
+        return True
 
-for links in lista:
-    print(links)
-
-print('o primeiro item da lista é: ', lista[0])
-print("o tamanho da lista completa é: ", len(lista))
 
 while True:
-    if lista_aux[0] != lista[0]:
-        print('há atualização')
-        lista_aux.append(lista[0])
-        time.sleep(15000)
+    for link in soup.find_all("a", {"class": "thumbnail"}):
+        lista.append(link.get('href'))
+
+    last_url = lista[0]
+    new_url = lista_aux[0]
+    a = get_equals(last_url, new_url)
+    if a != True:
+        for links in lista:
+            bot.sendMessage(1142090567, links)
+            lista_aux[0] = lista[0]
+    
+    time.sleep(15)
+
