bcf24f8a7be384a7338327a9bfd11708cbbc4a90
andre-bandeli <andre.lbandeli@gmail.com>
2021-08-30 13:44:03 -0300

    logica para receber atualizaçao

--- scrp.py
+++ scrp.py
@@ -4,6 +4,7 @@ import requests
 page = requests.get("https://empregacampinas.com.br/?s=marketing")
 soup = BeautifulSoup(page.content, 'html.parser')
 lista = []
+lista_aux = [0]
 
 for link in soup.find_all("a", {"class": "thumbnail"}):
     lista.append(link.get('href'))
@@ -12,6 +13,9 @@ for links in lista:
     print(links)
 
 print('o primeiro item da lista é: ', lista[0])
-
 print("o tamanho da lista completa é: ", len(lista))
 
+while True:
+    if lista_aux[0] != lista[0]:
+        print('há atualização')
+        lista_aux.append(lista[0])
