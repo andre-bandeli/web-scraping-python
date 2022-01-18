2341dfb33b345f65d5cda5033361d06abb236b31
André L. Bandeli <87938869+andre-bandeli@users.noreply.github.com>
2022-01-18 04:48:32 -0300

    Add files via upload

---
+++ scrap_vagas.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import telepot
+
+bot_chatID = 'your id'
+bot = telepot.Bot('your code')
+
+page = requests.get("https://www.vagas.com.br/vagas-de-manutencao")
+soup = BeautifulSoup(page.content, 'html.parser')
+lista = []
+lista_aux = [0]
+
+def get_equals(str1, str2):
+    str1 = lista[0]
+    str2 = lista_aux[0]
+    if str1 == str2:
+        return True
+
+
+while True:
+    for link in soup.find_all("a", {"class": "link-detalhes-vaga"}):
+        lista.append(link.get('href'))
+
+    last_url = lista[0]
+    new_url = lista_aux[0]
+    a = get_equals(last_url, new_url)
+
+    if a != True:
+        for links in lista:
+            #bot.sendMessage(1142090567, links)
+            lista_aux[0] = lista[0]
+            print(links)
+    time.sleep(15)
+
